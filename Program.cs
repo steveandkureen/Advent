9beb9e259588a116a982469d077690dac6514a81
Stephen Schwarz <steve@steveandkureen.com>
2024-12-17 08:30:24 -0700

    day 16 complete

--- Program.cs
+++ Program.cs
@@ -26,20 +26,26 @@ using System.Xml.XPath;
 
 internal class Program
 {
-    const char UP = '^';
-    const char DOWN = 'v';
-    const char RIGHT = '>';
-    const char LEFT = '<';
-    const char ROBOT = '@';
     const char WALL = '#';
-    const char BOX = 'O';
     const char EMPTY = '.';
-    const char NEWBOX1 = '[';
-    const char NEWBOX2 = ']';
-    static List<char> Moves = new List<char>();
-    static List<List<char>> Map = new List<List<char>>();
+    const char START = 'S';
+    const char END = 'E';
 
-    static bool SimpleBox = false;
+
+    const int NORTH = 0;
+    const int SOUTH = 1;
+    const int EAST = 2;
+    const int WEST = 3;
+
+    static List<List<char>> Map = new List<List<char>>();
+    static List<Answer> Answers = new List<Answer>();
+
+    public class WalkerPosition
+    {
+        public Point Position { get; set; }
+        public int Direction { get; set; }
+
+    }
 
     private static void Main(string[] args)
     {
@@ -50,276 +56,266 @@ internal class Program
             data = reader.ReadToEnd();
         }
 
-        ParseMapData(data);
-        OutputMap();
-        var robot = FindRobot();
-        var moveCount = 0;
-        char lastmove = '-';
-        foreach (var move in Moves)
-        {
-            robot = MoveRobot(robot, move);
-            lastmove = move;
-            moveCount++;
-            if (moveCount == 19998)
-            {
-                OutputMap();
-            }
-            //Console.WriteLine("MoveCount: " + moveCount++);
-            //OutputMap();
-            //Thread.Sleep(100);
-        }
-        Console.WriteLine("LastMove: " + lastmove);
-        OutputMap();
-        int boxCount = 0;
-        for (int y = 0; y < Map.Count; y++)
-        {
-            var line = Map[y];
-            for (int x = 0; x < line.Count; x++)
-            {
-                char pos = line[x];
-                if (pos == NEWBOX2)
-                {
-                    boxCount++;
-                }
-            }
-        }
-        Console.WriteLine("Box Count: " + boxCount);
-        var answer = CalculateResult();
-        Console.WriteLine("Answer: " + answer);
+        Map = ParseMapData(data);
+        OutputMap(Map);
+        var start = FindPosition(START);
+        var end = FindPosition(END);
+        var bestAnswer = AStar(new WalkerPosition { Position = start, Direction = EAST }, Map, end);
+        if (bestAnswer == null)
+            return;
+
+        var answers = new List<Point>(bestAnswer.Path);
+        var answer = FindNewPath(bestAnswer, data, bestAnswer, start, end, answers);
+
+        Console.WriteLine("Answer: " + (answers.Count + 1));
     }
 
-    private static object CalculateResult()
-    {
-        var result = 0;
-        var boxCount = 0;
-        for (int y = 0; y < Map.Count; y++)
-        {
-            var line = Map[y];
-            var width = Map[y].Count();
-            for (int x = 0; x < line.Count; x++)
-            {
-                char pos = line[x];
-                if (pos == BOX || pos == NEWBOX1)
-                {
-                    result += (100 * y) + x;
-                    boxCount++;
-                }
-            }
-        }
-        Console.WriteLine("BoxCount in Score: " + boxCount);
-        return result;
-    }
-
-    private static Point FindRobot()
+    private static int ShowFinalAnswer(List<Point> points)
     {
         for (int y = 0; y < Map.Count; y++)
         {
-            var line = Map[y];
+            List<char>? line = Map[y];
             for (int x = 0; x < line.Count; x++)
             {
                 char pos = line[x];
-                if (pos == ROBOT)
+                var p = new Point(x, y);
+                if (points.Contains(p) || pos == START)
                 {
+                    Console.Write('O');
+                }
+                else
+                {
+                    Console.Write(pos);
+                }
+
+            }
+            Console.Write("\n");
+        }
+        return points.Count;
+    }
+
+    private static int FindNewPath(Answer currentAnswer, string mapData, Answer bestAnswer, Point start, Point end, List<Point> totalList)
+    {
+        var points = new Stack<Point>(currentAnswer.Path);
+        currentAnswer.Path.ForEach(p => points.Push(p));
+
+        while (points.Count > 0)
+        {
+            var pos = points.Pop();
+            if (pos == start || pos == end)
+                continue;
+            var currentMap = ParseMapData(mapData);
+            currentMap[pos.Y][pos.X] = WALL;
+            var newAnswer = AStar(new WalkerPosition { Position = start, Direction = EAST }, currentMap, end);
+            //OutputMap(currentMap);
+            if (newAnswer != null && newAnswer.Cost == bestAnswer.Cost)
+            {
+                //OutputMap(currentMap);
+                var newMap = SerializeMap(currentMap);
+                var added = false;
+                newAnswer.Path.ForEach(p =>
+                {
+                    if (!totalList.Contains(p))
+                    {
+                        added = true;
+                        totalList.Add(p);
+                    }
+                });
+                if (added)
+                {
+                    FindNewPath(newAnswer, newMap, bestAnswer, start, end, totalList);
+                }
+            }
+        }
+        ShowFinalAnswer(totalList);
+        return totalList.Count + 1;
+    }
+
+    private static string SerializeMap(List<List<char>> map)
+    {
+        var builder = new StringBuilder();
+        foreach (var line in map)
+        {
+            foreach (var c in line)
+            {
+                builder.Append(c);
+            }
+            builder.Append('\n');
+        }
+        return builder.ToString();
+    }
+
+    private static int Heuristic(Point a, Point b)
+    {
+        return Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
+    }
+
+    private static Answer? AStar(WalkerPosition start, List<List<char>> map, Point end)
+    {
+        var frontier = new PriorityQueue<WalkerPosition, int>();
+        frontier.Enqueue(start, 0);
+        var cameFrom = new Dictionary<Point, Point?>();
+        var costSoFar = new Dictionary<Point, int>();
+        cameFrom[start.Position] = null;
+        costSoFar[start.Position] = 0;
+
+        while (frontier.Count != 0)
+        {
+            var current = frontier.Dequeue();
+
+            if (current.Position == end)
+            {
+                return CalculateAnswer(cameFrom, start, end, costSoFar[end]);
+            }
+
+            foreach (var next in GetNeighbors(current, map))
+            {
+                var newCost = costSoFar[current.Position] + CalculateCost(current, next);
+
+                if (!costSoFar.ContainsKey(next.Position) || newCost < costSoFar[next.Position])
+                {
+                    costSoFar[next.Position] = newCost;
+                    var priority = newCost + Heuristic(end, next.Position);
+                    frontier.Enqueue(next, priority);
+                    cameFrom[next.Position] = current.Position;
+                }
+            }
+        }
+        return null;
+    }
+
+    public class Answer
+    {
+        public List<Point> Path { get; set; } = new List<Point>();
+        public int Cost { get; set; }
+    }
+
+    private static Answer CalculateAnswer(Dictionary<Point, Point?> cameFrom, WalkerPosition start, Point end, int cost)
+    {
+        var answer = new Answer();
+        Point? current = end;
+        while (current != null && cameFrom[current.Value] != null)
+        {
+            answer.Path.Add(current.Value);
+            current = cameFrom[current.Value];
+        }
+        answer.Cost = cost;
+        return answer;
+    }
+
+    private static int CalculateCost(WalkerPosition current, WalkerPosition next)
+    {
+        var cost = 1;
+        if (current.Direction == next.Direction)
+            return cost;
+
+        if (current.Direction == NORTH || current.Direction == SOUTH)
+        {
+            if (next.Direction == EAST || next.Direction == WEST)
+            {
+                cost += 1000;
+            }
+            else
+            {
+                cost += 2000;
+            }
+        }
+        else if (current.Direction == WEST || current.Direction == EAST)
+        {
+            if (next.Direction == NORTH || next.Direction == SOUTH)
+            {
+                cost += 1000;
+            }
+            else
+            {
+                cost += 2000;
+            }
+        }
+        return cost;
+    }
+
+    private static List<WalkerPosition> GetNeighbors(WalkerPosition current, List<List<char>> map)
+    {
+        var pos = current.Position;
+        var neighbors = new List<WalkerPosition>();
+        CheckNeighbor(current, new Point(pos.X + 1, pos.Y), map, neighbors);
+        CheckNeighbor(current, new Point(pos.X - 1, pos.Y), map, neighbors);
+        CheckNeighbor(current, new Point(pos.X, pos.Y + 1), map, neighbors);
+        CheckNeighbor(current, new Point(pos.X, pos.Y - 1), map, neighbors);
+        return neighbors;
+    }
+
+    private static void CheckNeighbor(WalkerPosition current, Point point, List<List<char>> map, List<WalkerPosition> neighbors)
+    {
+        if (map[point.Y][point.X] == EMPTY || map[point.Y][point.X] == END)
+        {
+            var direction = NORTH;
+            if (current.Position.X > point.X)
+            {
+                direction = WEST;
+            }
+            else if (current.Position.X < point.X)
+            {
+                direction = EAST;
+            }
+            else if (current.Position.Y > point.Y)
+            {
+                direction = SOUTH;
+            }
+            else if (current.Position.Y < point.Y)
+            {
+                direction = NORTH;
+            }
+            neighbors.Add(new WalkerPosition { Position = point, Direction = direction });
+        }
+    }
+
+    private static Point FindPosition(char c)
+    {
+        for (int y = 0; y < Map.Count; y++)
+        {
+            List<char>? line = Map[y];
+            for (int x = 0; x < line.Count; x++)
+            {
+                if (Map[y][x] == c)
                     return new Point(x, y);
-                }
             }
         }
         return new Point();
     }
 
-    private static Point MoveRobot(Point robot, char move)
+    private static void OutputMap(List<List<char>> map)
     {
-        var newPos = robot;
-        newPos = Move(move, newPos);
-        if (CheckMove(newPos, move, false))
+        for (int y = 0; y < map.Count; y++)
         {
-            CheckMove(newPos, move, true);
-            Map[newPos.Y][newPos.X] = '@';
-            Map[robot.Y][robot.X] = '.';
-            return newPos;
-        }
-        return robot;
-    }
-
-    private static Point Move(char move, Point currentPos)
-    {
-        var newPos = currentPos;
-        switch (move)
-        {
-            case UP:
-                newPos.Y -= 1;
-                break;
-            case DOWN:
-                newPos.Y += 1;
-                break;
-            case LEFT:
-                newPos.X -= 1;
-                break;
-            case RIGHT:
-                newPos.X += 1;
-                break;
-        }
-
-        return newPos;
-    }
-
-    private static bool CheckMove(Point newPos, char direction, bool doMove)
-    {
-        switch (Map[newPos.Y][newPos.X])
-        {
-            case WALL:
-                return false;
-            case EMPTY:
-                return true;
-            case BOX:
-            case NEWBOX1:
-            case NEWBOX2:
-                return PushBox(newPos, direction, doMove) ? true : false;
-        }
-        return false;
-    }
-
-    private static bool PushBox(Point newPos, char direction, bool doMove)
-    {
-        if (SimpleBox)
-            return PushBoxHorizontal(newPos, direction, doMove);
-
-        if (direction == UP || direction == DOWN)
-        {
-            return PushBoxVertical(newPos, direction, doMove);
-        }
-        return PushBoxHorizontal(newPos, direction, doMove);
-    }
-
-    private static bool PushBoxHorizontal(Point newPos, char direction, bool doMove)
-    {
-        var newBox = Move(direction, newPos);
-        if (CheckMove(newBox, direction, doMove))
-        {
-            if (doMove)
+            List<char>? line = map[y];
+            for (int x = 0; x < line.Count; x++)
             {
-                Map[newBox.Y][newBox.X] = Map[newPos.Y][newPos.X];
-            }
-            return true;
-        }
-        return false;
-    }
-
-    private static bool PushBoxVertical(Point newPos, char direction, bool doMove)
-    {
-        var newBox = Move(direction, newPos);
-        Point boxLeft, boxRight;
-        if (Map[newPos.Y][newPos.X] == ']')
-        {
-            boxRight = newBox;
-            boxLeft = new Point(newBox.X - 1, newBox.Y);
-        }
-        else
-        {
-            boxRight = new Point(newBox.X + 1, newBox.Y);
-            boxLeft = newBox;
-        }
-
-        if (CheckMove(boxRight, direction, doMove) && CheckMove(boxLeft, direction, doMove))
-        {
-            if (doMove)
-            {
-                if (Map[newPos.Y][newPos.X] == ']')
+                char pos = line[x];
                 {
-                    Map[boxLeft.Y][boxLeft.X] = Map[newPos.Y][newPos.X - 1];
-                    Map[boxRight.Y][boxRight.X] = Map[newPos.Y][newPos.X];
-                    Map[newPos.Y][newPos.X - 1] = '.';
-                    Map[newPos.Y][newPos.X] = '.';
+                    Console.Write(pos);
                 }
-                else
-                {
-                    Map[boxLeft.Y][boxLeft.X] = Map[newPos.Y][newPos.X];
-                    Map[boxRight.Y][boxRight.X] = Map[newPos.Y][newPos.X + 1];
-                    Map[newPos.Y][newPos.X] = '.';
-                    Map[newPos.Y][newPos.X + 1] = '.';
-                }
-            }
-            return true;
-        }
-        return false;
-    }
 
-    private static void OutputMap()
-    {
-        foreach (var line in Map)
-        {
-            foreach (var pos in line)
-            {
-                Console.Write(pos);
             }
             Console.Write("\n");
         }
     }
 
-    private static void ParseMapData(string data)
+    private static List<List<char>> ParseMapData(string data)
     {
-        Console.WriteLine("Box Count: " + data.Where(c => c == 'O').Count());
-        bool readMap = true;
+        var map = new List<List<char>>();
         foreach (var line in data.Split("\n"))
         {
-            if (string.IsNullOrEmpty(line.Trim()))
+            var newLine = new List<char>();
+            foreach (var c in line.ToCharArray())
             {
-                readMap = false;
-                continue;
-            }
+                newLine.AddRange([c]);
 
-            if (readMap)
-            {
-                var newLine = new List<char>();
-                foreach (var c in line.ToCharArray())
-                {
-                    if (SimpleBox)
-                    {
-                        newLine.AddRange([c]);
-                    }
-                    else
-                    {
-                        newLine.AddRange(TranslateChars(c));
-                    }
-
-                }
-                Map.Add(newLine);
             }
-            else
-            {
-                Moves.AddRange(line.Trim().ToCharArray());
-            }
+            map.Add(newLine);
         }
-        int boxCount = 0;
-        for (int y = 0; y < Map.Count; y++)
-        {
-            var line = Map[y];
-            for (int x = 0; x < line.Count; x++)
-            {
-                char pos = line[x];
-                if (pos == NEWBOX1)
-                {
-                    boxCount++;
-                }
-            }
-        }
-        Console.WriteLine("Box Count: " + boxCount);
+        return map;
     }
 
-    private static List<char> TranslateChars(char c)
-    {
-        switch (c)
-        {
-            case WALL:
-                return ['#', '#'];
-            case BOX:
-                return ['[', ']'];
-            case EMPTY:
-                return ['.', '.'];
-            case ROBOT:
-                return ['@', '.'];
-        }
-        return ['.'];
-    }
 }
 
