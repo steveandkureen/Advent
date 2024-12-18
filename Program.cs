10d325a8ae063b16d3ff0758a365a2d985584da6
Stephen Schwarz <steve@steveandkureen.com>
2024-12-18 08:36:04 -0700

    stage 1 day 16 complete

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ using System.Numerics;
 using System.Reflection.Metadata;
 using System.Reflection.Metadata.Ecma335;
 using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics.Arm;
 using System.Runtime.Serialization.Formatters;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -26,26 +27,28 @@ using System.Xml.XPath;
 
 internal class Program
 {
-    const char WALL = '#';
-    const char EMPTY = '.';
-    const char START = 'S';
-    const char END = 'E';
 
-
-    const int NORTH = 0;
-    const int SOUTH = 1;
-    const int EAST = 2;
-    const int WEST = 3;
-
-    static List<List<char>> Map = new List<List<char>>();
-    static List<Answer> Answers = new List<Answer>();
-
-    public class WalkerPosition
+    public enum OPCODES
     {
-        public Point Position { get; set; }
-        public int Direction { get; set; }
-
+        ADV = 0,
+        BXL = 1,
+        BST = 2,
+        JNZ = 3,
+        BXC = 4,
+        OUT = 5,
+        BDV = 6,
+        CDV = 7
     }
+
+    static long RegA = 0;
+    static long RegB = 0;
+    static long RegC = 0;
+
+    static bool found = false;
+
+
+    static char[] Instructions = new char[0];
+
 
     private static void Main(string[] args)
     {
@@ -56,266 +59,203 @@ internal class Program
             data = reader.ReadToEnd();
         }
 
-        Map = ParseMapData(data);
-        OutputMap(Map);
-        var start = FindPosition(START);
-        var end = FindPosition(END);
-        var bestAnswer = AStar(new WalkerPosition { Position = start, Direction = EAST }, Map, end);
-        if (bestAnswer == null)
-            return;
+        ReadInitData(data);
+        OutputComputer();
 
-        var answers = new List<Point>(bestAnswer.Path);
-        var answer = FindNewPath(bestAnswer, data, bestAnswer, start, end, answers);
-
-        Console.WriteLine("Answer: " + (answers.Count + 1));
-    }
-
-    private static int ShowFinalAnswer(List<Point> points)
-    {
-        for (int y = 0; y < Map.Count; y++)
+        var tasks = new List<Task>();
+        var threadCount = 10;
+        var chunk = (281474900000000 - 35185000000000) / threadCount;
+        for (var i = 0; i < threadCount; i++)
         {
-            List<char>? line = Map[y];
-            for (int x = 0; x < line.Count; x++)
-            {
-                char pos = line[x];
-                var p = new Point(x, y);
-                if (points.Contains(p) || pos == START)
-                {
-                    Console.Write('O');
-                }
-                else
-                {
-                    Console.Write(pos);
-                }
-
-            }
-            Console.Write("\n");
+            long start = 0;
+            start = 35185000000000 + (chunk * i);
+            Console.WriteLine("Chunck: " + i + " " + start);
+            tasks.Add(CreateTask(start, start, RegB, RegC, start + chunk, i));
+            //new Task(() => { RunBatch(start, start, RegB, RegC, start + chunk, i); }));
         }
-        return points.Count;
+        tasks.ForEach(t => t.Start());
+        Task.WaitAll(tasks.ToArray());
+        //3372036854775807 - min
+        // bool done = false;
+        // long chunk = 1;
+        // long regA = RegA;
+        // long regB = RegA;
+        // long regC = RegC;
+        // long stop = long.MaxValue;
+        // long thread = 0;
+        // long currentTry = 10000000000000;
+        //10000000000000;
+        //1000000000000000
+        //min 35185000000000
+        //max 281474900000000
+        // Console.WriteLine($"Thread Config: {thread}, {currentTry}, {regA}, {regB}, {regC}, {stop}");
+        // var output = new List<long>();
+        // while (currentTry < stop && !found)
+        // {
+        //     output.Clear();
+        //     RunComputer(currentTry, regB, regC, output);
+        //     //Console.WriteLine("Length: " + currentTry + " " + output.Count);
+        //     if (output.Count < Instructions.Length)
+        //     {
+        //         currentTry += 1000000000;
+        //         // return;
+        //     }
+        //     else if (output.Count > Instructions.Length)
+        //     {
+        //         currentTry -= 100000000;
+        //         //Console.WriteLine("Length: " + currentTry);
+
+        //     }
+        //     else
+        //     {
+        //         return;
+        //     }
+        //     // if (CompareOutput(output))
+        //     // {
+        //     //     Console.WriteLine(currentTry);
+        //     //     return;
+        //     // }
+        //     //currentTry++;
+        //     // if (currentTry % 1000000 == 0)
+        //     // {
+        //     //     Console.WriteLine("CurrentTry: " + currentTry + " " + thread);
+        //     // }
+        // }
+
+
     }
 
-    private static int FindNewPath(Answer currentAnswer, string mapData, Answer bestAnswer, Point start, Point end, List<Point> totalList)
+    private static Task CreateTask(long currentTry, long start, long regB, long regC, long chunk, int i)
     {
-        var points = new Stack<Point>(currentAnswer.Path);
-        currentAnswer.Path.ForEach(p => points.Push(p));
+        return new Task(() => { RunBatch(currentTry, start, RegB, RegC, start + chunk, i); });
+    }
 
-        while (points.Count > 0)
+    private static bool RunBatch(Int64 currentTry, long regA, long regB, long regC, long stop, long thread)
+    {
+        Console.WriteLine($"Thread Config: {thread}, {currentTry}, {regA}, {regB}, {regC}, {stop}");
+        var output = new List<long>();
+        while (currentTry < stop && !found)
         {
-            var pos = points.Pop();
-            if (pos == start || pos == end)
-                continue;
-            var currentMap = ParseMapData(mapData);
-            currentMap[pos.Y][pos.X] = WALL;
-            var newAnswer = AStar(new WalkerPosition { Position = start, Direction = EAST }, currentMap, end);
-            //OutputMap(currentMap);
-            if (newAnswer != null && newAnswer.Cost == bestAnswer.Cost)
+            output.Clear();
+            RunComputer(currentTry, regB, regC, output);
+            if (CompareOutput(output))
             {
-                //OutputMap(currentMap);
-                var newMap = SerializeMap(currentMap);
-                var added = false;
-                newAnswer.Path.ForEach(p =>
-                {
-                    if (!totalList.Contains(p))
+                Console.WriteLine(currentTry);
+                return true;
+            }
+            currentTry++;
+            if (currentTry % 1000000 == 0)
+            {
+                Console.WriteLine("CurrentTry: " + currentTry + " " + thread);
+            }
+        }
+        return false;
+    }
+
+    private static bool CompareOutput(List<long> output)
+    {
+        if (output.Count != Instructions.Length)
+            return false;
+
+        for (int i = 0; i < Instructions.Length; i++)
+        {
+            if (long.Parse([Instructions[i]]) != output[i])
+                return false;
+        }
+        Console.WriteLine("Insructions: " + string.Join(",", Instructions));
+        Console.WriteLine("Output: " + string.Join(",", output));
+        found = true;
+        return true;
+    }
+
+    private static void RunComputer(long regA, long regB, long regC, List<long> output)
+    {
+        long currentInstruction = 0;
+        while (currentInstruction < Instructions.Length)
+        {
+            var operand = long.Parse([Instructions[currentInstruction + 1]]);
+            var instruction = (OPCODES)long.Parse([Instructions[currentInstruction]]);
+            switch (instruction)
+            {
+                case OPCODES.ADV:
+                    regA /= (long)Math.Pow(2, GetComboOperand(operand, regA, regB, regC));
+                    break;
+                case OPCODES.BXL:
+                    regB = regB ^ operand;
+                    break;
+                case OPCODES.BST:
+                    regB = GetComboOperand(operand, regA, regB, regC) % 8;
+                    break;
+                case OPCODES.JNZ:
+                    if (regA != 0)
                     {
-                        added = true;
-                        totalList.Add(p);
+                        currentInstruction = operand;
+                        continue;
                     }
-                });
-                if (added)
-                {
-                    FindNewPath(newAnswer, newMap, bestAnswer, start, end, totalList);
-                }
+                    break;
+                case OPCODES.BXC:
+                    regB = regB ^ regC;
+                    break;
+                case OPCODES.OUT:
+                    output.Add(GetComboOperand(operand, regA, regB, regC) % 8);
+                    break;
+                case OPCODES.BDV:
+                    regB = regA / (long)Math.Pow(2, GetComboOperand(operand, regA, regB, regC));
+                    break;
+                case OPCODES.CDV:
+                    regC = regA / (long)Math.Pow(2, GetComboOperand(operand, regA, regB, regC));
+                    break;
             }
+            currentInstruction += 2;
         }
-        ShowFinalAnswer(totalList);
-        return totalList.Count + 1;
     }
 
-    private static string SerializeMap(List<List<char>> map)
+    private static long GetComboOperand(long operand, long regA, long regB, long regC)
     {
-        var builder = new StringBuilder();
-        foreach (var line in map)
+        switch (operand)
         {
-            foreach (var c in line)
-            {
-                builder.Append(c);
-            }
-            builder.Append('\n');
-        }
-        return builder.ToString();
-    }
-
-    private static int Heuristic(Point a, Point b)
-    {
-        return Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
-    }
-
-    private static Answer? AStar(WalkerPosition start, List<List<char>> map, Point end)
-    {
-        var frontier = new PriorityQueue<WalkerPosition, int>();
-        frontier.Enqueue(start, 0);
-        var cameFrom = new Dictionary<Point, Point?>();
-        var costSoFar = new Dictionary<Point, int>();
-        cameFrom[start.Position] = null;
-        costSoFar[start.Position] = 0;
-
-        while (frontier.Count != 0)
-        {
-            var current = frontier.Dequeue();
-
-            if (current.Position == end)
-            {
-                return CalculateAnswer(cameFrom, start, end, costSoFar[end]);
-            }
-
-            foreach (var next in GetNeighbors(current, map))
-            {
-                var newCost = costSoFar[current.Position] + CalculateCost(current, next);
-
-                if (!costSoFar.ContainsKey(next.Position) || newCost < costSoFar[next.Position])
-                {
-                    costSoFar[next.Position] = newCost;
-                    var priority = newCost + Heuristic(end, next.Position);
-                    frontier.Enqueue(next, priority);
-                    cameFrom[next.Position] = current.Position;
-                }
-            }
-        }
-        return null;
-    }
-
-    public class Answer
-    {
-        public List<Point> Path { get; set; } = new List<Point>();
-        public int Cost { get; set; }
-    }
-
-    private static Answer CalculateAnswer(Dictionary<Point, Point?> cameFrom, WalkerPosition start, Point end, int cost)
-    {
-        var answer = new Answer();
-        Point? current = end;
-        while (current != null && cameFrom[current.Value] != null)
-        {
-            answer.Path.Add(current.Value);
-            current = cameFrom[current.Value];
-        }
-        answer.Cost = cost;
-        return answer;
-    }
-
-    private static int CalculateCost(WalkerPosition current, WalkerPosition next)
-    {
-        var cost = 1;
-        if (current.Direction == next.Direction)
-            return cost;
-
-        if (current.Direction == NORTH || current.Direction == SOUTH)
-        {
-            if (next.Direction == EAST || next.Direction == WEST)
-            {
-                cost += 1000;
-            }
-            else
-            {
-                cost += 2000;
-            }
-        }
-        else if (current.Direction == WEST || current.Direction == EAST)
-        {
-            if (next.Direction == NORTH || next.Direction == SOUTH)
-            {
-                cost += 1000;
-            }
-            else
-            {
-                cost += 2000;
-            }
-        }
-        return cost;
-    }
-
-    private static List<WalkerPosition> GetNeighbors(WalkerPosition current, List<List<char>> map)
-    {
-        var pos = current.Position;
-        var neighbors = new List<WalkerPosition>();
-        CheckNeighbor(current, new Point(pos.X + 1, pos.Y), map, neighbors);
-        CheckNeighbor(current, new Point(pos.X - 1, pos.Y), map, neighbors);
-        CheckNeighbor(current, new Point(pos.X, pos.Y + 1), map, neighbors);
-        CheckNeighbor(current, new Point(pos.X, pos.Y - 1), map, neighbors);
-        return neighbors;
-    }
-
-    private static void CheckNeighbor(WalkerPosition current, Point point, List<List<char>> map, List<WalkerPosition> neighbors)
-    {
-        if (map[point.Y][point.X] == EMPTY || map[point.Y][point.X] == END)
-        {
-            var direction = NORTH;
-            if (current.Position.X > point.X)
-            {
-                direction = WEST;
-            }
-            else if (current.Position.X < point.X)
-            {
-                direction = EAST;
-            }
-            else if (current.Position.Y > point.Y)
-            {
-                direction = SOUTH;
-            }
-            else if (current.Position.Y < point.Y)
-            {
-                direction = NORTH;
-            }
-            neighbors.Add(new WalkerPosition { Position = point, Direction = direction });
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+                return operand;
+            case 4:
+                return regA;
+            case 5:
+                return regB;
+            case 6:
+                return regC;
+            case 7:
+            default:
+                throw new InvalidOperationException("Operand 7 not allowed");
         }
     }
 
-    private static Point FindPosition(char c)
+    private static void OutputComputer()
     {
-        for (int y = 0; y < Map.Count; y++)
-        {
-            List<char>? line = Map[y];
-            for (int x = 0; x < line.Count; x++)
-            {
-                if (Map[y][x] == c)
-                    return new Point(x, y);
-            }
-        }
-        return new Point();
+        Console.WriteLine("Register A: " + RegA);
+        Console.WriteLine("Register B: " + RegB);
+        Console.WriteLine("Register C: " + RegC);
+
+        Console.WriteLine("Program: " + string.Join(',', Instructions));
     }
 
-    private static void OutputMap(List<List<char>> map)
+    private static void ReadInitData(string data)
     {
-        for (int y = 0; y < map.Count; y++)
-        {
-            List<char>? line = map[y];
-            for (int x = 0; x < line.Count; x++)
-            {
-                char pos = line[x];
-                {
-                    Console.Write(pos);
-                }
+        var lines = data.Split("\n");
+        RegA = ReadRegister(lines[0].Trim());
+        RegB = ReadRegister(lines[1].Trim());
+        RegC = ReadRegister(lines[2].Trim());
 
-            }
-            Console.Write("\n");
-        }
+        var instructions = lines[4].Trim();
+        instructions = instructions.Substring(instructions.IndexOf(":") + 1).Trim();
+        Instructions = instructions.Split(",").Select(c => char.Parse(c)).ToArray();
     }
 
-    private static List<List<char>> ParseMapData(string data)
+    private static long ReadRegister(string line)
     {
-        var map = new List<List<char>>();
-        foreach (var line in data.Split("\n"))
-        {
-            var newLine = new List<char>();
-            foreach (var c in line.ToCharArray())
-            {
-                newLine.AddRange([c]);
-
-            }
-            map.Add(newLine);
-        }
-        return map;
+        long colonIndex = line.IndexOf(":");
+        return long.Parse(line.Substring((int)colonIndex + 1, (int)(line.Length - colonIndex - 1)));
     }
-
 }
 
